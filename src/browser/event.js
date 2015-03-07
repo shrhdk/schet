4ba68dbb1f2c075a5c90617d52810cc54afcd101
Hideki Shiro <hideki@shiro.be>
2015-03-07 21:58:19 +0900

    Fix up design

--- src/browser/event.js
+++ src/browser/event.js
@@ -27,6 +27,10 @@ $(() => {
     this.textContent = iso8601.prettify(this.textContent.trim(), moment().utcOffset());
   });
 
+  // auto size textarea
+
+  autosize(document.getElementById('description'));
+
   // Fix and Unfix Event
 
   $('.fix-event').change(function () {
@@ -41,6 +45,63 @@ $(() => {
     }
   });
 
+  // Add Comment
+  $('#comment-form').submit(() => {
+    let name = $('#comment-name').val();
+    let body = $('#comment-body').val();
+
+    schet.addComment(name, body).then(() => location.reload())
+      .catch(err => {
+        switch (err.status) {
+          case 400:
+            return alert('Bad Comment');
+        }
+        location.reload();
+      });
+
+    return false;
+  });
+
+  // Edit Comment Name
+  $('.comment').each(function () {
+    let commentID = this.getAttribute('comment-id');
+
+    // Edit Comment Name
+    $(this).find('.comment-name').editable((elem, fallback)=> {
+      let name = elem.text();
+      schet.updateComment(commentID, {name}).then(event => {
+        elem.text(event.comments[commentID].name);
+      }).catch(err => {
+        switch (err.status) {
+          case 400:
+            alert('Bad Comment');
+            return fallback();
+        }
+        location.reload();
+      });
+    });
+
+    // Edit Comment Body
+    $(this).find('.comment-body').editable((elem, fallback) => {
+      let body = elem.text();
+      schet.updateComment(commentID, {body}).then(event => {
+        elem.text(event.comments[commentID].body);
+      }).catch(err => {
+        switch (err.status) {
+          case 400:
+            alert('Bad Comment');
+            return fallback();
+        }
+      });
+    });
+
+    // Delete Comment
+    $(this).find('.delete-comment').click(() => {
+      schet.deleteComment(commentID).then(() => location.reload())
+        .catch(() => location.reload());
+    });
+  });
+
   // The following code make page modifiable.
   // So, skip the followings when the event is fixed.
   if (isFixed) {
@@ -48,7 +109,7 @@ $(() => {
   }
 
   // Edit Event Title
-  $('#title').editable((elem, fallback, abort) => {
+  $('#title').editable((elem, fallback) => {
     let title = elem.text();
     schet.update({title}).then(event => {
       document.title = event.title;
@@ -68,20 +129,8 @@ $(() => {
 
   // Edit Event Description
 
-  autosize(document.getElementById('description'));
-
   $('#description').focus(function () {
     let snapshot = this.value;
-
-    const fallback = () => {
-      this.value = snapshot;
-      this.focus();
-    };
-
-    const cancel = () => {
-      this.value = snapshot;
-      this.blur();
-    };
 
     // Cancel (by ESC)
     $(this).keydown(ev => {
@@ -89,7 +138,8 @@ $(() => {
         return;
       }
 
-      cancel();
+      this.value = snapshot;
+      this.blur();
     });
 
     // Fix (by Blur)
@@ -97,11 +147,11 @@ $(() => {
       let description = this.value;
       schet.update({description}).then(event => {
         this.value = event.description;
+
+        // Update Textarea Size
+        this.dispatchEvent(document.createEvent('Event').initEvent('autosize.update', true, false));
       }).catch(err => {
         switch (err.status) {
-          case 400:
-            alert('Bad Description.');
-            return fallback();
           case 409:
             alert('Event is already fixed.');
             break;
@@ -196,7 +246,14 @@ $(() => {
   $('.delete-term').click(function () {
     let termID = this.getAttribute('term-id');
     schet.deleteTerm(termID).then(() => location.reload())
-      .catch(() => location.reload());
+      .catch(err => {
+        switch (err.status) {
+          case 409:
+            alert('Event is already fixed.');
+            break;
+        }
+        location.reload();
+      });
   });
 
   // Edit Record
@@ -205,62 +262,13 @@ $(() => {
     let termID = this.getAttribute('term-id');
 
     schet.updateParticipant(participantID, {[termID]: this.value})
-      .catch(() => location.reload());
-  });
-
-  // Add Comment
-  $('#comment-form').submit(() => {
-    let name = $('#comment-name').val();
-    let body = $('#comment-body').val();
-
-    schet.addComment(name, body).then(() => location.reload())
       .catch(err => {
         switch (err.status) {
-          case 400:
-            alert(err.message);
+          case 409:
+            alert('Event is already fixed.');
             break;
-          case 404:
-            alert(err.message);
-            location.reload();
-            break;
-          default:
-            location.reload();
         }
+        location.reload();
       });
-
-    return false;
-  });
-
-  // Edit Comment Name
-  $('.comment').each(function () {
-    let commentID = this.getAttribute('comment-id');
-
-    // Edit Comment Name
-    $(this).find('.comment-name').editable((elem, fallback)=> {
-      let name = elem.text();
-      schet.updateComment(commentID, {name}).then(event => {
-        elem.text(event.comments[commentID].name);
-      }).catch(err => {
-        alert(err);
-        fallback();
-      });
-    });
-
-    // Edit Comment Body
-    $(this).find('.comment-body').editable((elem, fallback) => {
-      let body = elem.text();
-      schet.updateComment(commentID, {body}).then(event => {
-        elem.text(event.comments[commentID].body);
-      }).catch(err => {
-        alert(err);
-        fallback();
-      });
-    });
-
-    // Delete Comment
-    $(this).find('.delete-comment').click(() => {
-      schet.deleteComment(commentID).then(() => location.reload())
-        .catch(() => location.reload());
-    });
   });
 });
