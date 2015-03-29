5884aac177ea8e6695a7069f6620c7adcd74094f
Hideki Shiro <hideki@shiro.be>
2015-03-30 01:58:26 +0900

    fix test

--- test/util/form.js
+++ test/util/form.js
@@ -5,8 +5,10 @@ var GiveAndExpect = require('../test-helper').GiveAndExpect;
 
 var form = require('../../src/util/form.js');
 
-describe('strictTrim', () => {
-  let test = new GiveAndExpect(form.strictTrim);
-  it('trim whitespace',
-    test.give(form.WHITE_SPACE + 'foo' + form.WHITE_SPACE).expect('foo'));
+describe('form', () => {
+  describe('strictTrim', () => {
+    let test = new GiveAndExpect(form.strictTrim);
+    it('trim whitespace',
+      test.give(form.WHITE_SPACE + 'foo' + form.WHITE_SPACE).expect('foo'));
+  });
 });
